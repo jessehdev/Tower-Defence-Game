d2187cb322dd8e6e5f60dbde6c8b1ea50d6c2164
Jesse <jesse.heikinheimo@gmail.com>
2024-02-07 22:25:57 +0200

    Added enemy movement and tower shooting

--- src/main/scala/enemies/Enemy.scala
+++ src/main/scala/enemies/Enemy.scala
@@ -2,24 +2,33 @@ package enemies
 
 import utils.{GridPos}
 import utils.{Constants}
+import game.{TowerDefenceGame}
 
 trait Enemy {
   //abstracts
-  val health: Int
+  var health: Int
   val damage: Int
   val attackSpeed: Int
   var gridPos: GridPos
 //added this, wasn't in the original plan
-  var pathIndex: Int
+  var pathIndex: Int 
 //concretes
 //constants should eventually be replaced with reading from JSON
   val constants = new Constants
   val enemyPath = constants.enemyPath
-
+  val game = new TowerDefenceGame
+  val enemies = game.enemies
+  
   def getPos() = 
     this.gridPos
- 
-  def move() = ???
- 
-  def takeDamage() = ???
+  end getPos
+
+  def move() = 
+    pathIndex += 1
+    this.gridPos = enemyPath(pathIndex).gridPos
+  end move
+  
+  //added the construction parameter
+  def takeDamage(amountOfDamage: Int) = 
+    this.health = amountOfDamage
 }
--- src/main/scala/towers/Tower.scala
+++ src/main/scala/towers/Tower.scala
@@ -1,14 +1,31 @@
 package towers
 import utils.{GridPos}
+import enemies.{Enemy}
+import scala.collection.mutable.ArrayBuffer
+import scala.math.{pow, sqrt}
 
-trait Tower(val position: GridPos) {
+trait Tower {
+  val position: GridPos
   var health: Int
   var damage: Int
   val firingRate: Int
-  val range: Int
+  val range: Double
   val cost: Int
 
-  def shootEnemy() = ???
-  
+  // if enemy if range, choose an arbitrary one to shoot
+  def shootEnemy(arr: ArrayBuffer[Enemy]) = 
+    var inRange = ArrayBuffer[Enemy]()
+    arr.foreach( enemy =>
+      val dx = ( this.position.x - enemy.gridPos.x ).abs.toDouble
+      val dy = ( this.position.y - enemy.gridPos.y ).abs.toDouble
+      val squared = pow(dx, 2.0) + pow(dy, 2.0)
+      if sqrt(squared) <= this.range then
+        inRange += enemy
+    )
+    if inRange.nonEmpty then
+      val target = inRange.head
+      target.takeDamage(this.damage)
+   end shootEnemy
+
   def ugrade(): Unit 
 }
