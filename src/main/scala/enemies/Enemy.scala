13886c1be31c243f43cb593b78b5e9bbb3994267
Jesse <jesse.heikinheimo@gmail.com>
2024-02-08 22:48:05 +0200

    added game instance in enemy, enemy class now complete for mvp

--- src/main/scala/enemies/Enemy.scala
+++ src/main/scala/enemies/Enemy.scala
@@ -12,23 +12,18 @@ trait Enemy {
   var position: GridPos
 //added this, wasn't in the original plan
   var pathIndex: Int 
-//concretes
-//constants should eventually be replaced with reading from JSON
-  val constants = new Constants
-  val enemyPath = constants.enemyPath
-  val game = new TowerDefenceGame
-  val enemies = game.enemies
-  
+
   def getPos() = 
     this.position
   end getPos
 
-  def move() = 
+  // added construction parameter
+  def move(game: TowerDefenceGame) = 
     pathIndex += 1
-    this.position = enemyPath(pathIndex).gridPos
+    this.position = game.enemyPath(pathIndex).gridPos
   end move
   
   //added the construction parameter
   def takeDamage(amountOfDamage: Int) = 
-    this.health = amountOfDamage
+    this.health -= amountOfDamage
 }
